56edbdae45a9711926364a50bc58e1a16668b41e
Alex Rozumey <brain-geek@yandex.ua>
2013-06-11 00:02:14 +0300

    Changing benchmark script

--- bench-script.sh
+++ bench-script.sh
@@ -5,9 +5,9 @@ FOLDER=$2
 
 mkdir -p $FOLDER
 
-for i in $(seq 20); do
-  echo "Running $((5*$i)) browsers per process experiment (using 4 processes)"
+for i in $(seq 30); do
+  echo "Running $(($i)) browsers per process experiment (using 4 processes)"
 
   wget http://$IP/sitemap
-  xvfb-run ./toad -u http://$IP -t 180 -p 4 -c $(($i * 3)) -o $FOLDER/log$i.log
+  xvfb-run ./toad -u http://$IP -t 180 -p 4 -c $(($i)) -o $FOLDER/log$i.log
 done
